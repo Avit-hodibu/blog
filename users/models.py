7f63652399de3b129b678a65642b66697b1281b2
Avit Rai Hodibu <59275766+Avit-hodibu@users.noreply.github.com>
2022-04-28 10:39:48 +0545

    Update models.py

--- users/models.py
+++ users/models.py
@@ -12,8 +12,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    def save(self, *args, **kawrgs):
+        super().save(*args, **kawrgs)
         
         img = Image.open(self.image.path)
         
